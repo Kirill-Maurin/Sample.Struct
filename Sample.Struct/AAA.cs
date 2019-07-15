c05987086fe0d7f9ad13f4f495445a2899226efe
Kirill Maurin <leo.bonart@gmail.com>
2019-07-16 01:17:29 +0300

    Add Arrange-Act-Assert support

---
+++ Sample.Struct/AAA.cs
@@ -0,0 +1,30 @@
+using System;
+
+public static class AAA
+{
+    public static ArrangeResult<T> Arrange<T>(this T @object) => new ArrangeResult<T>(@object);
+    
+    public static ActResult<T> Act<T>(this ArrangeResult<T> arrange, Action<T> act)
+    {
+        act(arrange.Object);
+        return new ActResult<T>(arrange.Object);
+    }
+
+    public static ActResult<TResult> Act<T, TResult>(this ArrangeResult<T> arrange, Func<T, TResult> act)        
+        => new ActResult<TResult>(act(arrange.Object));   
+
+    public static void Assert<T>(this ActResult<T> act, Action<T> assert) 
+        => assert(act.Object); 
+
+    public readonly struct ArrangeResult<T>
+    {
+        internal ArrangeResult(T @object) => Object = @object;
+        internal T Object { get; } 
+    }
+
+    public readonly struct ActResult<T>
+    {
+        internal ActResult(T @object) => Object = @object;
+        internal T Object { get; }         
+    }    
+}

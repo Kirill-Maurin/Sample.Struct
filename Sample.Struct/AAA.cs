2a74165d67073b55ce17d52f516b926b25605ff1
Kirill Maurin <k.maurin@sirena-travel.ru>
2019-07-16 09:13:28 +0300

    Add Arrange-Act-Assert support

--- Sample.Struct/AAA.cs
+++ Sample.Struct/AAA.cs
@@ -1,30 +1,33 @@
 using System;
 
-public static class AAA
+namespace Sample.Struct
 {
-    public static ArrangeResult<T> Arrange<T>(this T @object) => new ArrangeResult<T>(@object);
-    
-    public static ActResult<T> Act<T>(this ArrangeResult<T> arrange, Action<T> act)
+    public static class AAA
     {
-        act(arrange.Object);
-        return new ActResult<T>(arrange.Object);
+        public static ArrangeResult<T> Arrange<T>(this T @object) => new ArrangeResult<T>(@object);
+
+        public static ActResult<T> Act<T>(this ArrangeResult<T> arrange, Action<T> act)
+        {
+            act(arrange.Object);
+            return new ActResult<T>(arrange.Object);
+        }
+
+        public static ActResult<TResult> Act<T, TResult>(this ArrangeResult<T> arrange, Func<T, TResult> act)
+            => new ActResult<TResult>(act(arrange.Object));
+
+        public static void Assert<T>(this ActResult<T> act, Action<T> assert)
+            => assert(act.Object);
+
+        public readonly struct ArrangeResult<T>
+        {
+            internal ArrangeResult(T @object) => Object = @object;
+            internal T Object { get; }
+        }
+
+        public readonly struct ActResult<T>
+        {
+            internal ActResult(T @object) => Object = @object;
+            internal T Object { get; }
+        }
     }
-
-    public static ActResult<TResult> Act<T, TResult>(this ArrangeResult<T> arrange, Func<T, TResult> act)        
-        => new ActResult<TResult>(act(arrange.Object));   
-
-    public static void Assert<T>(this ActResult<T> act, Action<T> assert) 
-        => assert(act.Object); 
-
-    public readonly struct ArrangeResult<T>
-    {
-        internal ArrangeResult(T @object) => Object = @object;
-        internal T Object { get; } 
-    }
-
-    public readonly struct ActResult<T>
-    {
-        internal ActResult(T @object) => Object = @object;
-        internal T Object { get; }         
-    }    
 }

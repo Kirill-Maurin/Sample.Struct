e49c624090061ae50676c3d836a28fd5a83b0942
Kirill Maurin <leo.bonart@gmail.com>
2019-07-30 03:03:47 +0300

    Optimize ArrayEnumerator

--- Sample.Struct/Enumerables/ArrayEnumerator.cs
+++ Sample.Struct/Enumerables/ArrayEnumerator.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Sample.Struct.Enumerables
 {
     public struct ArrayEnumerator<T> : IEnumerator<T>
     {
-        internal ArrayEnumerator(T[] array) => (_i, _array) = (-1, array);
+        internal ArrayEnumerator(T[] array) => (_i, _array, Current) = (-1, array, default);
 
         readonly T[] _array;
 
         int _i;
 
-        public T Current => _array[_i];
+        public T Current { get; private set; }
 
         object IEnumerator.Current => Current;
 
         public void Dispose() {}
 
-        public bool MoveNext() => ++_i < _array.Length;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool MoveNext()
+        {            
+            if (++_i >= _array.Length)
+                return false;
+            Current = _array[_i];
+            return true;
+        }
 
         public void Reset() => throw new System.NotImplementedException();
     }

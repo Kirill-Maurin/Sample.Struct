13701638f641c434d67d2c21574e2b4319318971
Kirill Maurin <leo.bonart@gmail.com>
2019-08-18 21:22:56 +0300

    Separate sum and equals benchmarks

--- Sample.Struct.Benchmarks/ArrayEqualBenchmarks.cs
+++ Sample.Struct.Benchmarks/ArrayEqualBenchmarks.cs
@@ -1,9 +1,7 @@
 ﻿using System.Linq;
 using BenchmarkDotNet.Attributes;
-using Sample.Struct.Enumerables;
 using Sample.Struct.Equatables;
 using Sample.Struct.Indexables;
-using Sample.Struct.Summators;
 
 namespace Sample.Struct.Benchmarks
 {
@@ -45,34 +43,7 @@ namespace Sample.Struct.Benchmarks
             }
 
             return true;
-        }
-
-        [Benchmark(Baseline = true)]
-        public void IEnumerableIntSumArrayBenchmark() => _array.Sum();
-
-        [Benchmark]
-        public void ForEachSumArrayBenchmark() => ForeachSum(_array);
-
-        [Benchmark]
-        public void ForSumArrayBenchmark() => ForSum(_array);
-
-        [Benchmark]
-        public void GenericIEnumerableIntSumArrayBenchmark() => _array.AsEnumerable().AsStructEnumerable().Sum();
-
-        [Benchmark]
-        public void GenericEnumerableIntSumArrayBenchmark() => _array.AsStructEnumerable().Sum();
-
-        [Benchmark]
-        public void GenericEnumerableIntCheckedSumArrayBenchmark() => _array.AsStructEnumerable().Sum(0.AsCheckedAdditive());
-
-        [Benchmark]
-        public void GenericEnumerableSumArrayBenchmark() => _array.AsStructEnumerable().Sum(0.AsAdditive());
-
-        [Benchmark]
-        public void GenericEnumerableLongSumArrayBenchmark() => _array.AsStructEnumerable().Sum(0.AsAccumulator());
-
-        [Benchmark]
-        public void GenericIndexableSumArrayBenchmark() => _array.AsIndexable().Sum(0.AsAdditive());
+        }        
 
         static int[] _array = Enumerable.Range(0, 1000).ToArray();
 
@@ -81,21 +52,5 @@ namespace Sample.Struct.Benchmarks
         static object[] _objArray = _array.Cast<object>().ToArray();
 
         static object[] _objArray2 = _objArray.ToArray();
-
-        public static int ForeachSum(int[] array)
-        {
-            var sum = 0;
-            foreach (var n in array)
-                sum += n;
-            return sum;
-        }
-
-        public static int ForSum(int[] array)
-        {
-            var sum = 0;
-            for (var i = 0; i < array.Length; i++)
-                sum += array[i];
-            return sum;
-        }
     }
 }
---
+++ Sample.Struct.Benchmarks/ArraySumBenchmarks.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Sample.Struct.Enumerables;
+using Sample.Struct.Indexables;
+using Sample.Struct.Summators;
+
+namespace Sample.Struct.Benchmarks
+{
+    public class ArraySumBenchmarks
+    {
+        [Benchmark(Baseline = true)]
+        public void LinqSumArrayBenchmark() => _array.Sum();
+
+        public void IEnumerableIntSumArrayBenchmark() => Sum(_array);
+
+        public static int Sum(IEnumerable<int> source)
+        {
+            int sum = 0;
+            checked
+            {
+                foreach (int v in source)
+                    sum += v;
+            }
+            return sum;
+        }
+
+        [Benchmark]
+        public void ForEachSumArrayBenchmark() => ForeachSum(_array);
+
+        [Benchmark]
+        public void ForSumArrayBenchmark() => ForSum(_array);
+
+        [Benchmark]
+        public void GenericIEnumerableIntSumArrayBenchmark() => _array.AsEnumerable().AsStructEnumerable().Sum();
+
+        [Benchmark]
+        public void GenericEnumerableIntSumArrayBenchmark() => _array.AsStructEnumerable().Sum();
+
+        [Benchmark]
+        public void GenericEnumerableIntCheckedSumArrayBenchmark() => _array.AsStructEnumerable().Sum(0.AsCheckedAdditive());
+
+        [Benchmark]
+        public void GenericEnumerableSumArrayBenchmark() => _array.AsStructEnumerable().Sum(0.AsAdditive());
+
+        [Benchmark]
+        public void GenericEnumerableLongSumArrayBenchmark() => _array.AsStructEnumerable().Sum(0.AsAccumulator());
+
+        [Benchmark]
+        public void GenericIndexableSumArrayBenchmark() => _array.AsIndexable().Sum(0.AsAdditive());
+
+        static int[] _array = Enumerable.Range(0, 1000).ToArray();
+
+        public static int ForeachSum(int[] array)
+        {
+            var sum = 0;
+            foreach (var n in array)
+                sum += n;
+            return sum;
+        }
+
+        public static long ForSum(int[] array)
+        {
+            var sum = 0L;
+            for (var i = 0; i < array.Length; i++)
+                sum += array[i];
+            return sum;
+        }
+    }
+}

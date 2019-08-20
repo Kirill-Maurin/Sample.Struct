ffc2869754736dec06f46c0496aec9d2bacbe234
Kirill Maurin <leo.bonart@gmail.com>
2019-08-20 03:42:01 +0300

    Extract checked int summator

---
+++ Sample.Struct/Summators/IntCheckedSummator.cs
@@ -0,0 +1,7 @@
+﻿namespace Sample.Struct.Summators
+{
+    public struct IntCheckedSummator : ISummator<int>
+    {
+        public int Add(int left, int right) { checked { return left + right; } }
+    }
+}
--- Sample.Struct/Summators/IntSummator.cs
+++ Sample.Struct/Summators/IntSummator.cs
@@ -9,9 +9,4 @@
     {
         public long Add(long left, int right) => left + right;
     }
-
-    public struct IntCheckedSummator : ISummator<int>
-    {
-        public int Add(int left, int right) { checked { return left + right; } }
-    }
 }

2d457460a8c648e014992aa378edf93108297a53
ihor.volokhovych <ihor.volokhovych@pm.bet>
2023-07-21 18:52:10 +0300

    feat: refactored indexables

--- Sample.Struct/Indexables/ArrayIndexable.cs
+++ Sample.Struct/Indexables/ArrayIndexable.cs
@@ -1,28 +1,20 @@
-﻿namespace Sample.Struct.Indexables
+﻿namespace Sample.Struct.Indexables;
+
+public static class ArrayIndexable
 {
-    public static class ArrayIndexable
+    public static Indexable<T, int, T[], ArrayIndexator<T>> AsGenericIndexable<T>(this T[] array)
     {
-        public static Indexable<T, int, T[], ArrayIndexator<T>> AsGenericIndexable<T>(this T[] array)
-            => array;
-
-        public static Indexable<T, int, ArrayIndexable<T>, DefaultIndexator<T, int, ArrayIndexable<T>>> AsStructIndexable<T>(this T[] array)
-            => new ArrayIndexable<T>(array);
-
-        public static Indexable<T, int, ArrayIndexable<T>> AsIndexable<T>(this T[] array)
-            => new Indexable<T, int, ArrayIndexable<T>>(array);
+        return array;
     }
 
-    public struct ArrayIndexable<T> : IIndexable<T, int>
+    public static Indexable<T, int, ArrayIndexable<T>, DefaultIndexator<T, int, ArrayIndexable<T>>>
+        AsStructIndexable<T>(this T[] array)
     {
-        internal ArrayIndexable(T[] value) => Value = value;
+        return new ArrayIndexable<T>(array);
+    }
 
-        public T[] Value { get; }
-
-        public T this[int index] => Value[index];
-
-        public int Count => Value.Length;
-
-        public static implicit operator ArrayIndexable<T>(T[] array) => new ArrayIndexable<T>(array);
-        public static implicit operator T[](ArrayIndexable<T> value) => value.Value;
+    public static Indexable<T, int, ArrayIndexable<T>> AsIndexable<T>(this T[] array)
+    {
+        return new Indexable<T, int, ArrayIndexable<T>>(array);
     }
 }
---
+++ Sample.Struct/Indexables/ArrayIndexableT.cs
@@ -0,0 +1,25 @@
+﻿namespace Sample.Struct.Indexables;
+
+public struct ArrayIndexable<T> : IIndexable<T, int>
+{
+    internal ArrayIndexable(T[] value)
+    {
+        this.Value = value;
+    }
+
+    public T[] Value { get; }
+
+    public T this[int index] => this.Value[index];
+
+    public int Count => this.Value.Length;
+
+    public static implicit operator ArrayIndexable<T>(T[] array)
+    {
+        return new ArrayIndexable<T>(array);
+    }
+
+    public static implicit operator T[](ArrayIndexable<T> value)
+    {
+        return value.Value;
+    }
+}

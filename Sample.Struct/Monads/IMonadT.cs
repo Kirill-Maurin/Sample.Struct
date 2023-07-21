2848cacdaa6625391e7df2360465e280e9a01fb7
ihor.volokhovych <ihor.volokhovych@pm.bet>
2023-07-21 18:53:11 +0300

    feat: refactored monads

---
+++ Sample.Struct/Monads/IMonadT.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample.Struct.Monads;
+
+using System;
+
+public interface IMonad<TMonad, T>
+{
+    TOutMonad Bind<TOut, TInMonad, TOutMonad>(TInMonad m, Func<T, TOutMonad> action)
+        where TOutMonad : IMonad<TMonad, TOut>
+        where TInMonad : IMonad<TMonad, TOut>;
+}

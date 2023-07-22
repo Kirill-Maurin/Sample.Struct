1f76d067b10ac8d3c50fccf97d254d9d8ad4ce27
Ihor Volokhovych <igorvolokhovych@gmail.com>
2023-07-22 17:53:55 +0300

    feat: Sample.Struct main files to scoped namespace (#12)
    feat: Sample.Struct main files to scoped namespace (#12)

--- Sample.Struct/NotNull.cs
+++ Sample.Struct/NotNull.cs
@@ -1,49 +1,50 @@
-﻿using System;
-using Sample.Struct.Options;
+﻿namespace Sample.Struct;
 
-namespace Sample.Struct
+using System;
+
+public static class NotNull
 {
-    public static class NotNull
+    public static NotNull<T> EnsureNotNull<T>(this NotNull<T> value)
     {
-        public static NotNull<T> EnsureNotNull<T>(this NotNull<T> value) => value;
-        public static NotNull<T> EnsureNotNull<T>([NotNull]this T value) => value.EnsureNotNull(string.Empty);
-        public static NotNull<T> EnsureNotNull<T>([NotNull]this T value, string name)
-        {
-            if (value == null)
-                throw new ArgumentNullException(name);
-            return new NotNull<T>(value);
-        }
-        public static NotNull<T> AsNotNull<T>(this T value) where T: struct => new NotNull<T>(value);
-        public static NotNull<T> AsNotNull<T>(this NotNull<T> value) where T: struct => value;
-        public static NotNull<T> CannotBeNull<T>([NotNull]this T reference) where T: class => new NotNull<T>(reference);
-
-        public static bool TryCreate<T>(T value, out NotNull<T> notNull)
-        {
-            if (value == null)
-            {
-                notNull = default;
-                return false;
-            }
-
-            notNull = new NotNull<T>(value);
-            return true;
-        }
+        return value;
     }
 
-    public readonly struct NotNull<T> : IEquatable<NotNull<T>>
+    public static NotNull<T> EnsureNotNull<T>([NotNull] this T value)
     {
-        internal NotNull([NotNull]T unwrap) => Value = unwrap;
+        return value.EnsureNotNull(string.Empty);
+    }
 
-        [NotNull]
-        public T Value { get; }
+    public static NotNull<T> EnsureNotNull<T>([NotNull] this T value, string name)
+    {
+        if (value == null)
+            throw new ArgumentNullException(name);
+        return new NotNull<T>(value);
+    }
 
-        public override int GetHashCode() => Value.GetHashCode();
-        public override string ToString() => $"NotNull {Value}";
-        public override bool Equals(object obj) => obj is NotNull<T> notNull && Equals(notNull);
-        public bool Equals(NotNull<T> other) => Value.Equals(other.Value);
-        public static bool operator ==(NotNull<T> left, NotNull<T> right) => left.Equals(right);
-        public static bool operator !=(NotNull<T> left, NotNull<T> right) => !left.Equals(right);
-        public static implicit operator T(NotNull<T> t) => t.Value;
-        public static implicit operator NotNull<T>(T value) => value.EnsureNotNull();
+    public static NotNull<T> AsNotNull<T>(this T value) where T : struct
+    {
+        return new NotNull<T>(value);
+    }
+
+    public static NotNull<T> AsNotNull<T>(this NotNull<T> value) where T : struct
+    {
+        return value;
+    }
+
+    public static NotNull<T> CannotBeNull<T>([NotNull] this T reference) where T : class
+    {
+        return new NotNull<T>(reference);
+    }
+
+    public static bool TryCreate<T>(T value, out NotNull<T> notNull)
+    {
+        if (value == null)
+        {
+            notNull = default;
+            return false;
+        }
+
+        notNull = new NotNull<T>(value);
+        return true;
     }
 }

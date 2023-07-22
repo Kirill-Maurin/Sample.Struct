9ad3057add8918863063f97eb767113faa109021
Ihor Volokhovych <igorvolokhovych@gmail.com>
2023-07-22 17:42:00 +0300

    feat: Indexables to scoped namespace (#6)
    * feat: changed nuget to net7
    
    * feat: fix editorconfig and issues with new versions
    
    * feat: refactored indexables
    
    ---------
    
    Co-authored-by: ihor.volokhovych <ihor.volokhovych@pm.bet>

---
+++ Sample.Struct/Indexables/IIndexator.cs
@@ -0,0 +1,7 @@
+﻿namespace Sample.Struct.Indexables;
+
+public interface IIndexator<out T, TIndex, in TIndexable>
+{
+    TIndex GetCount(TIndexable indexable);
+    T GetItem(TIndexable indexable, TIndex index);
+}

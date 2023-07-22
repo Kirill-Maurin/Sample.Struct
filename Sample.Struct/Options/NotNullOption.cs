e77b926128b8d123fd2a8d5c5ebd736158a537ae
Ihor Volokhovych <igorvolokhovych@gmail.com>
2023-07-22 17:47:14 +0300

    [8] feat: Options to scoped namespace (#9)
    [8] feat: Options to scoped namespace

--- Sample.Struct/Options/NotNullOption.cs
+++ Sample.Struct/Options/NotNullOption.cs
@@ -1,31 +1,9 @@
-﻿using System;
+﻿namespace Sample.Struct.Options;
 
-namespace Sample.Struct.Options
+public static class NotNullOption
 {
-    public static class NotNullOption
+    public static Option<T, NotNullOption<T>> AsOption<T>(this NotNull<T> notNull) where T : class
     {
-        public static Option<T, NotNullOption<T>> AsOption<T>(this NotNull<T> notNull) where T : class => new Option<T, NotNullOption<T>>(new NotNullOption<T>(notNull));
-    }
-
-    public readonly struct NotNullOption<T> : IOption<T>, IEquatable<NotNullOption<T>>
-    {
-        internal NotNullOption(NotNull<T> value) => Value = value;
-
-        NotNull<T> Value { get; }
-
-        public bool TryGetValue(out NotNull<T> value)
-        {
-            value = Value;
-            return true;
-        }
-
-        public bool HasValue => true;
-
-        public override int GetHashCode() => Value.GetHashCode();
-        public override string ToString() => Value.ToString();
-        public override bool Equals(object obj) => obj is NotNullOption<T> option && Equals(option);
-        public bool Equals(NotNullOption<T> other) => Value.Equals(other.Value);
-        public static bool operator ==(NotNullOption<T> left, NotNullOption<T> right) => left.Equals(right);
-        public static bool operator !=(NotNullOption<T> left, NotNullOption<T> right) => !left.Equals(right);
+        return new Option<T, NotNullOption<T>>(new NotNullOption<T>(notNull));
     }
 }
--- Sample.Struct/Options/OptionStruct.cs
+++ Sample.Struct/Options/OptionStruct.cs
@@ -1,37 +1,14 @@
-using System;
+namespace Sample.Struct.Options;
 
-namespace Sample.Struct.Options
+public static class OptionStruct
 {
-    public static class OptionStruct
+    public static OptionStruct<T> ToOptionStruct<T>(this T value)
     {
-        public static OptionStruct<T> ToOptionStruct<T>(this T value) => value == null ? Null<T>() : new OptionStruct<T>(value);
-        public static OptionStruct<T> Null<T>() => OptionStruct<T>.Null;
+        return value == null ? Null<T>() : new OptionStruct<T>(value);
     }
 
-    public readonly struct OptionStruct<T> : IOption<T>, IEquatable<OptionStruct<T>>
+    public static OptionStruct<T> Null<T>()
     {
-        internal OptionStruct(in T value) => (HasValue, Value) = (true, value);
-
-        public bool HasValue { get; }
-
-        public bool TryGetValue(out NotNull<T> value)
-        {
-            value = Value;
-            return HasValue;
-        }
-
-        NotNull<T> Value { get; }
-
-        internal static OptionStruct<T> Null { get; } = new OptionStruct<T>();
-
-        public bool Equals(OptionStruct<T> option) => HasValue == option.HasValue && (!HasValue || Value.Equals(option.Value));
-
-        public override bool Equals(object obj) => obj is OptionStruct<T> option && Equals(option);
-
-        public override int GetHashCode() => HasValue ? Value.GetHashCode() : 2019994878;
-
-        public static bool operator ==(OptionStruct<T> left, OptionStruct<T> right) => left.Equals(right);
-
-        public static bool operator !=(OptionStruct<T> left, OptionStruct<T> right) => !(left == right);
+        return OptionStruct<T>.Null;
     }
 }
